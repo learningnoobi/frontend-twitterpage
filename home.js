a424e2deb0086ba268e13f66a85f6b2ec621a696
learningnoobi <64244098+learningnoobi@users.noreply.github.com>
2021-01-14 23:51:32 +0545

    homepage

---
+++ home.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+  document.getElementById('nav').classList.toggle("sidebar");
+}
+
+function closeNav() {
+  document.getElementById("nav").classList.toggle("sidebar");
+ 
+}
